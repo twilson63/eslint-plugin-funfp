0af0b1f2ff0a04b73d3653c55083a8997943346a
Tom Wilson <tom@jackhq.com>
2017-01-07 20:46:27 -0500

    fixed module exports

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const reqAll = require('req-all')
-const createIndex = require('create-eslint-index')
-
-const rules = reqAll('rules', {camelize: false})
-
-const externalRecommendedRules = {
-  'no-var': 'error'
-}
-
-const internalRecommendedRules = createIndex.createConfig({
-  plugin: 'funfp',
-  field: 'meta.docs.recommended'
-}, rules)
-
 module.exports = {
-  rules,
+  rules: {
+    'no-array-map': require('./rules/no-array-map'),
+    'no-foreach': require('./rules/no-foreach')
+  },
   configs: {
-    rules: Object.assign({}, internalRecommendedRules, externalRecommendedRules)
+    recommended: {
+      rules: {
+        'no-array-map': "error",
+        'no-foreach': "error"
+      }
+    }
   }
 }
